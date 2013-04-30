d6d7332c760e1ced8f29ab6e21894b2dbb896347
DBC Apprentice <apprentice@devbootcamp.com>
2013-04-29 20:30:08 -0700

    mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,27 +9,32 @@ end
 
 post '/' do
 
-  p1_username = params['p1_username']
-  p2_username = params['p2_username']
+  # p1_username = params['p1_username']
+  # p2_username = params['p2_username']
 
-  p1 = Player.find_by_username(p1_username)
-  unless p1
-    p1 = Player.create(:username => p1_username) 
-  end
+  # p1 = Player.find_by_username(p1_username)
+  # unless p1
+  #   p1 = Player.create(:username => p1_username) 
+  # end
 
-  p2 = Player.find_by_username(p2_username)
-  unless p2
-    p2 = Player.create(:username => p2_username)
-  end
+  # p2 = Player.find_by_username(p2_username)
+  # unless p2
+  #   p2 = Player.create(:username => p2_username)
+  # end
 
-  game = Game.new
-  game.players << p1
-  game.players << p2
+  # game = Game.new
+  # game.players << p1
+  # game.players << p2
 
-  if game.valid? 
-    game.save
-    return "true"
-  else
-    return game.errors
-  end
+  # if game.valid? 
+  #   game.save
+  #   return "true"
+  # else
+  #   return game.errors
+  # end
+  return true
+end
+
+get '/outoforder' do
+  erb :outoforder
 end
--- public/js/application.js
+++ public/js/application.js
@@ -8,9 +8,122 @@ $(document).ready(function() {
     url: '/',
     data: {'p1_username' : p1_username, 'p2_username' : p2_username} 
   }).done(function(response) {
-      alert(response);
+      if (response === 'true') {
+
+      }
+      // alert(response);
     }).fail(function(a, b, c) {
-      alert("Game is broken.");
+      // alert("Game is broken.");
+      window.location = "/outoforder";
+    });
+
+    $('#p1').html(p1_username + " as Mario")
+    $('#p2').html(p2_username + " as Sonic")
+
+    var start = false;
+    var position1 = 1;
+    var position2 = 1;
+    var length_of_track = 20;
+
+    $(document).on('keyup', function(e) {
+      // Detect which key was pressed and call the appropriate function
+      // Google "jquery keyup what key was pressed" if you don't know how
+      if (e.keyCode==13){
+        start = true;
+      }
+
+      if (e.keyCode==32) {
+        var active = $('.active');
+        active.removeClass('active');
+        $('#player1_strip td:first-child').addClass('active');
+        $('#player2_strip td:first-child').addClass('active');
+        position1 = 1;
+        position2 = 1;
+        start = false;
+      }
+
+
+      if (start && (position1 < length_of_track) && (position2 < length_of_track)) {
+
+        if(e.keyCode==77){
+          
+          var active = $('#player1_strip .active');
+          active.removeClass('active');
+          active.next().addClass('active');
+          ++position1;
+          if (position1 == length_of_track) {
+            start = false;
+            alert("Mario wins!");
+          }
+        }
+        if (e.keyCode==83){
+          var active = $('#player2_strip .active');
+          active.removeClass('active');
+          active.next().addClass('active');
+          ++position2;
+          if (position2 == length_of_track) {
+            alert("Sonic, the muthafucking, HedgeHog wins!");
+            start = false;
+          }
+        }
+      }
+
+
     });
 
 });
+
+
+
+// $(document).ready(function() {
+//   var start = false;
+//   var position1 = 1;
+//   var position2 = 1;
+//   var length_of_track = 20;
+
+//   $(document).on('keyup', function(e) {
+//     // Detect which key was pressed and call the appropriate function
+//     // Google "jquery keyup what key was pressed" if you don't know how
+//     if (e.keyCode==13){
+//       start = true;
+//     }
+
+//     if (e.keyCode==32) {
+//       var active = $('.active');
+//       active.removeClass('active');
+//       $('#player1_strip td:first-child').addClass('active');
+//       $('#player2_strip td:first-child').addClass('active');
+//       position1 = 1;
+//       position2 = 1;
+//       start = false;
+//     }
+
+
+//     if (start && (position1 < length_of_track) && (position2 < length_of_track)) {
+
+//       if(e.keyCode==77){
+        
+//         var active = $('#player1_strip .active');
+//         active.removeClass('active');
+//         active.next().addClass('active');
+//         ++position1;
+//         if (position1 == length_of_track) {
+//           start = false;
+//           alert("Mario wins!");
+//         }
+//       }
+//       if (e.keyCode==83){
+//         var active = $('#player2_strip .active');
+//         active.removeClass('active');
+//         active.next().addClass('active');
+//         ++position2;
+//         if (position2 == length_of_track) {
+//           alert("Sonic, the muthafucking, HedgeHog wins!");
+//           start = false;
+//         }
+//       }
+//     }
+
+
+//   });
+// });
